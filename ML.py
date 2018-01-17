906b6d0effc8e2222a25f3067f81fbe7f565b419
Eric Guzman <eric.guzman75@gmail.com>
2018-01-17 12:43:16 +0100

    Update ML.py

--- ML.py
+++ ML.py
@@ -120,3 +120,15 @@ model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
 # summarize the model
 print(model.summary())
 
+# define problem
+vocab_size = 100
+max_length = 200
+# define model
+model = Sequential()
+model.add(Embedding(vocab_size, 100, input_length=max_length))
+model.add(Conv1D(filters=32, kernel_size=8, activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
+model.add(Flatten())
+model.add(Dense(10, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+print(model.summary())
